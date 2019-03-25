0b1cad763dcc8b74664168f54e21d76d0bdd300e
Henry418 <axel.gyrard@outlook.fr>
2019-03-25 08:37:21 +0100

    launchScreen + connection + rules + timerFragment added

---
+++ app/src/main/java/com/example/qrallye/ConnectionActivity.java
@@ -0,0 +1,22 @@
+package com.example.qrallye;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ConnectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection);
+
+        findViewById(R.id.connectBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RulesActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/qrallye/RulesActivity.java
@@ -0,0 +1,22 @@
+package com.example.qrallye;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RulesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+
+        findViewById(R.id.acceptRulesBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+    }
+}
