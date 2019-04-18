af3ce091eb881e65a5f4995995bc6c7ccb6db590
axelG <axel.gyrard@outlook.fr>
2019-04-18 13:59:34 +0200

    logout added

--- app/src/main/java/com/example/qrallye/ConnectionActivity.java
+++ app/src/main/java/com/example/qrallye/ConnectionActivity.java
@@ -1,14 +1,12 @@
 package com.example.qrallye;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.google.zxing.integration.android.IntentResult;
 
 public class ConnectionActivity extends AppCompatActivity {
 
@@ -20,28 +18,12 @@ public class ConnectionActivity extends AppCompatActivity {
         final EditText passText = findViewById(R.id.PasswordEdt);
         Button validateBtn = findViewById(R.id.validateBtn);
 
-        // if it is  alredy loged goes to the next activity
-        if(SessionMGR.getInstance().getLogedTeam() != null) goToNext();
-
         validateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SessionMGR.getInstance().login(userText.getText().toString().trim(), passText.getText().toString(), ConnectionActivity.this);
-                //goToNext();
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if(SessionMGR.getInstance().getLogedTeam() != null) goToNext();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(SessionMGR.getInstance().getLogedTeam() != null) goToNext();
     }
 
     public void goToNext(){
@@ -56,9 +38,9 @@ public class ConnectionActivity extends AppCompatActivity {
         }
     }
     public  void notifyWrongPasword(){
-        Toast.makeText(getApplicationContext(),"wrong password",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),getString(R.string.wrong_password),Toast.LENGTH_SHORT).show();
     }
     public  void notifyWrongUserName(){
-        Toast.makeText(getApplicationContext(),"wrong username",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(),getString(R.string.wrong_username),Toast.LENGTH_SHORT).show();
     }
 }
